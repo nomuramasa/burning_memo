90a3bcd01f950d812b21d7979a6c7294bac14bbe
nommy <nomuramasam@gmail.com>
2019-03-06 20:21:13 +0900

    最初の説明文を付けて、ページのどこかクリックで消えるようにした

--- scenery.js
+++
@@ -1,7 +0,0 @@
-// 朝と夜で景色が変わる
-var time = new Date().getHours() // 現在時刻を取得
-if(time <= 4 || time >= 18){ // 夜 18時00分〜4時59分
-  document.body.background = 'img/night.jpg' // 背景を夜空に
-}else{ // 朝 5時〜17時59分
-  document.body.background = 'img/sky.jpg' // 背景を青空に
-}
---
+++ various.js
@@ -0,0 +1,28 @@
+// いろんなことを書くJavaScriptファイル
+
+
+// 朝と夜で景色が変わる
+var time = new Date().getHours() // 現在時刻を取得
+
+if(time <= 4 || time >= 18){ // 夜 18時00分〜4時59分
+  document.body.background = 'img/night.jpg' // 背景を夜空に
+}else{ // 朝 5時〜17時59分
+  document.body.background = 'img/sky.jpg' // 背景を青空に
+}
+
+
+// 最初の説明文
+document.addEventListener('DOMContentLoaded', function(event) { 
+    if (sessionStorage.getItem('visited') == null) { // sessionにvisitedがまだ無いとき（つまりこのタブで最初のアクセスのとき）
+        document.getElementById('only_once').classList.add('d-block'); // 表示クラスを付ける
+        // document.getElementById('only_once').classList.remove('d-none'); //消すクラスをなくす
+        sessionStorage.setItem('visited', 1) // フラグを立てる
+    }
+});
+
+// どこかをクリックすると説明文は消える
+document.onclick=function(){
+  var	div = document.getElementById('only_once') // 説明文のdiv
+  var	container = document.getElementById('only_once').parentElement // その親のcontainer
+  container.removeChild(div) // conteinterからdivを削除
+}
